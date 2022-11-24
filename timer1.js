0d2dcd74ffce65c59aca5e683ebc923085552280
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-24 00:31:17 +0000

    removed comments

--- timer1.js
+++ timer1.js
@@ -1,11 +1,5 @@
 let alarms = process.argv.slice(2)
 
-
-// PLEASE READ, the else if statement prevents a string from being set as an alarm
-// This is because the alarm is a string which gets read as a number so adding strings to the if
-// statement would prevent the alarn from getting set in the first place.
-
-/// OMG javascript changing types gets very anoying
 for (let alarm of alarms) {
   //Edge cases
   if (alarm == 0 || alarm < 0 || typeof alarm == NaN) {
